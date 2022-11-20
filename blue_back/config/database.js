40a80869afef773dbb389aeca94fcd779bf41f79
Julio Buitrago <cesarbuitrago78@gmail.com>
2022-11-20 12:50:48 -0500

    Update Proyect

---
+++ blue_back/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const host = "127.0.0.1";
+const port = "27017";
+const db = "blueprint";
+
+exports.mongoConnect = () => {
+    const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
+    
+    mongoose.connect(mongoStringConnection);
+    mongoose.Promise = global.Promise;
+    const dbConnection = mongoose.connection;
+    dbConnection.on("error", console.error.bind
+    (console, "Mongodb connection error"))
+}
