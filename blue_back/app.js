ed6d7c807e35ecc146e4e64d091c8b4fd5c93f4c
Julio Buitrago <cesarbuitrago78@gmail.com>
2022-11-20 14:48:15 -0500

    Update Proyect

--- blue_back/app.js
+++ blue_back/app.js
@@ -5,14 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var database = require("./config/database");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+var empleadosRouter = require('./routes/empleados.router');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,8 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 database.mongoConnect();
 
 //ROUTER
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/empleados', empleadosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
